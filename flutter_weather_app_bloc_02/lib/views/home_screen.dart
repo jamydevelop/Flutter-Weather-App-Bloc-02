61db981ee29407ee9fd67aa6458618be89e59a6b
jamydevelop <127085144+jamydevelop@users.noreply.github.com>
2025-10-04 04:27:17 +0800

    fix Weather Icon

--- flutter_weather_app_bloc_02/lib/views/home_screen.dart
+++ flutter_weather_app_bloc_02/lib/views/home_screen.dart
@@ -12,6 +12,26 @@ class HomeScreen extends StatefulWidget {
 
 class _HomeScreenState extends State<HomeScreen> {
   final TextEditingController _controller = TextEditingController();
+  Widget getWeatherIcon(int code) {
+    switch (code) {
+      case >= 200 && < 300:
+        return Image.asset('assets/1.png');
+      case >= 300 && < 400:
+        return Image.asset('assets/2.png');
+      case >= 500 && < 600:
+        return Image.asset('assets/3.png');
+      case >= 600 && < 700:
+        return Image.asset('assets/4.png');
+      case >= 700 && < 800:
+        return Image.asset('assets/5.png');
+      case == 800:
+        return Image.asset('assets/6.png');
+      case > 800 && <= 804:
+        return Image.asset('assets/7.png');
+      default:
+        return Image.asset('assets/7.png');
+    }
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -54,7 +74,7 @@ class _HomeScreenState extends State<HomeScreen> {
                     const SizedBox(height: 20),
 
                     // Weather Icon
-                    Center(child: Image.asset('assets/3.png')),
+                    Center(child: getWeatherIcon(state.weather.weatherCodes)),
 
                     const SizedBox(height: 20),
 
